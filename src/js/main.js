8577cbf538f0242dbee7bc2fa5a864a93e22e3dc
Jakub Gerlée <jakub_gerlee@epam.com>
2020-02-03 14:38:57 +0100

    feat: Created simple-form with option to save message to local storage.

--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,37 @@
 // service worker registration - remove if you're not going to use it
 
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-        navigator.serviceWorker.register("serviceworker.js").then(
-            function(registration) {
-                // Registration was successful
-                console.log(
-                    "ServiceWorker registration successful with scope: ",
-                    registration.scope
-                );
-            },
-            function(err) {
-                // registration failed :(
-                console.log("ServiceWorker registration failed: ", err);
-            }
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
         );
-    });
+      },
+      function(err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
 }
+
+const saveButton = document.querySelector(".form__button--save--js");
+const loadButton = document.querySelector(".form__button--load--js");
+
+saveButton.addEventListener("click", e => {
+  e.preventDefault();
+  const message = document.querySelector("#message");
+  localStorage.setItem("message", JSON.stringify(message.value));
+});
+
+loadButton.addEventListener("click", e => {
+  e.preventDefault();
+  console.log(localStorage.getItem("message"));
+
+  let value = JSON.parse(localStorage.getItem("message"));
+  const message = document.querySelector("#message");
+  message.value = value;
+});
